2effc43f91e7f427d14b4ddbad21508ee0c4b351
Francois POIROTTE <francois.poirotte@c-s.fr>
2010-04-19 13:59:03 +0000

    Mise à jour du contrôleur RPC pour mieux prendre en compte les permissions sur les données. Ajout du support des permissions au proxy Nagios. Utilisation du proxy Nagios dans les résultats de la recherche (template searchhost.html). Mise à jour des données de test pour tester les permissions : - l'utilisateur "editor" voit le groupe "Serveurs" car il a accès au groupe "Serveurs Linux" (et uniquement celui-là) - dans "Serveurs Linux", il voit l'hôte "localhost" car il a accès à son service "SSH" (et uniquement celui-là) - il peut accéder à la page Nagios de "localhost" et voir tous les services configurés dessus - il peut accéder à la page Nagios du service "SSH" sur "localhost" - s'il tente d'accéder aux pages des autres services de "localhost", une erreur 403 lui est retournée - s'il tente d'accéder à un hôte non-supervisé ou qu'une autre erreur est rencontrée, il obtient une erreur 404
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@2750 b22e2e97-25c9-44ff-b637-2e5ceca36478

--- src/vigilo/turbogears/controllers/nagiosproxy.py
+++ src/vigilo/turbogears/controllers/nagiosproxy.py
@@ -4,19 +4,22 @@ Ce module agit comme un proxy qui va interroger les interfaces de Nagios
 et renvoyer le résultat pour qu'il puisse être affiché dans Vigilo.
 """
 
-import urllib, urllib2
+import urllib, urllib2, logging
 from tg import request, expose, config
 from tg.controllers import CUSTOM_CONTENT_TYPE
 import tg, pylons
+from repoze.what.predicates import not_anonymous
+from tg.exceptions import HTTPForbidden, HTTPNotFound
+from pylons.i18n import lazy_ugettext as l_
+from sqlalchemy import or_, and_
 
 from vigilo.models.session import DBSession
 from vigilo.models.tables import VigiloServer, Host, Ventilation, Application
+from vigilo.models.tables import SupItemGroup, LowLevelService
+from vigilo.models.tables.secondary_tables import SUPITEM_GROUP_TABLE
+from vigilo.turbogears.helpers import get_current_user
 
-class NoNagiosServerConfigured(Exception):
-    """
-    Exception levée lorsqu'aucun serveur Nagios configuré
-    pour un hôte donné n'a pu être trouvé.
-    """
+LOGGER = logging.getLogger(__name__)
 
 # pylint: disable-msg=R0201,W0232
 # - R0201: méthodes pouvant être écrites comme fonctions (imposé par TG2)
@@ -31,6 +34,8 @@ def make_nagios_proxy_controller(base_controller, mount_point):
     @type base_controller: C{BaseController}
     @param mount_point: URL à laquelle le contrôleur est montée
         (par rapport au RootController de l'application).
+        Ce paramètre est utilisé pour réécrire correctement les
+        liens absolus générés par Nagios.
     @type mount_point: C{basestring}
     @return: Instance du contrôleur agissant comme un proxy vers Nagios.
     @rtype: C{NagiosProxyController}
@@ -46,6 +51,9 @@ def make_nagios_proxy_controller(base_controller, mount_point):
         """
         Contrôleur agissant comme un proxy vers Nagios..
         """
+
+        # L'accès à ce contrôleur nécessite d'être identifié.
+        allow_only = not_anonymous(l_("You need to be authenticated"))
 
         def _get_server(self, host):
             """
@@ -94,9 +102,70 @@ def make_nagios_proxy_controller(base_controller, mount_point):
             @type kwargs: C{dict}
             """
 
+            user = get_current_user()
+            supitemgroups = user.supitemgroups(False)
+
+            # On vérifie qu'il existe effectivement un hôte portant ce nom
+            # et configuré pour être supervisé par Vigilo.
+            idhost = DBSession.query(
+                        Host.idhost
+                    ).filter(Host.name == host
+                    ).scalar()
+            if idhost is None:
+                LOGGER.warning(l_('No such monitored host: %s') % host)
+                raise HTTPNotFound()
+
+            # Éventuellement, l'utilisateur demande une page
+            # qui se rapporte à un service particulier.
+            service = kwargs.get('service')
+
+            # On regarde si l'utilisateur a accès à l'hôte
+            # et/ou au service demandé.
+            perm = DBSession.query(
+                        SupItemGroup.idgroup,
+                    ).join(
+                        (SUPITEM_GROUP_TABLE, SUPITEM_GROUP_TABLE.c.idgroup ==
+                            SupItemGroup.idgroup),
+                    ).outerjoin(
+                        (LowLevelService, LowLevelService.idservice ==
+                            SUPITEM_GROUP_TABLE.c.idsupitem),
+                    ).filter(SUPITEM_GROUP_TABLE.c.idgroup.in_(supitemgroups))
+
+            if service is not None:
+                perm = perm.filter(
+                            or_(
+                                and_(
+                                    LowLevelService.idhost == idhost,
+                                    LowLevelService.servicename == service,
+                                ),
+                                SUPITEM_GROUP_TABLE.c.idsupitem == idhost,
+                            ),
+                        )
+            else:
+                perm = perm.filter(
+                            or_(
+                                LowLevelService.idhost == idhost,
+                                SUPITEM_GROUP_TABLE.c.idsupitem == idhost,
+                            ),
+                        )
+
+            perm = perm.scalar()
+            if perm is None:
+                if service is not None:
+                    LOGGER.warning(l_('Access denied to host "%(host)s" and '
+                                    'service "%(service)s"') % {
+                        'host': host,
+                        'service': service,
+                    })
+                else:
+                    LOGGER.warning(l_('Access denied to host "%s"') % host)
+                raise HTTPForbidden()
+
             vigilo_server = self._get_server(host)
             if vigilo_server is None:
-                raise NoNagiosServerConfigured()
+                LOGGER.warning(l_('No Nagios server configured to '
+                                'monitor "%s"') % host)
+                raise HTTPNotFound()
 
             kwargs['host'] = host
             data = urllib.urlencode(kwargs)
