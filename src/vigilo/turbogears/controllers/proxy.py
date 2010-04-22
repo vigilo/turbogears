11c83a070fb5e1130dd69da7a716ff5588e6a9b2
Francois POIROTTE <francois.poirotte@c-s.fr>
2010-04-22 12:02:45 +0000

    Ajout d'une fonction "get_through_proxy" qui renvoie le document demandé concernant un hôte et un appli donnés, en utilisant le proxy. Le contrôleur proxy a été modifié pour utiliser cette méthode. Note de compatibilité : le contrôleur proxy n'ajoute plus automatiquement le paramètre "host" à la requête proxifiée. Il doit donc être retransmis manuellement.
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@2801 b22e2e97-25c9-44ff-b637-2e5ceca36478

--- src/vigilo/turbogears/controllers/proxy.py
+++ src/vigilo/turbogears/controllers/proxy.py
@@ -22,6 +22,159 @@ from vigilo.turbogears.helpers import get_current_user
 
 LOGGER = logging.getLogger(__name__)
 
+__all__ = ('make_proxy_controller', 'get_through_proxy', )
+
+def get_through_proxy(server_type, host, url, data=None, headers=None):
+    """
+    Récupère le contenu d'un document à travers le mécanisme de proxy.
+
+    @param server_type: Type d'application à "proxifier",
+        par exemple : "nagios" ou "rrdgraph".
+    @type server_type: C{basestring}
+    @param host: Nom de l'hôte (supervisé) concerné par la demande.
+    @type host: C{unicode}
+    @param url: URL à demander sur le serveur distant, avec éventuellement
+        des paramètres intégrés (query string).
+    @type url: C{basestring}
+    @param data: Dictionnaire contenant une série de paramètres à transmettre
+        dans la requête. Si des paramètres sont donnés, la requête engendrée
+        deviendra automatiquement du type POST au lieu de GET.
+    @type data: C{dict}
+    @param headers: Dictionnaire d'en-têtes HTTP à passer en plus dans la
+        requête. Vous pouvez par exemple utiliser l'en-tête 'X-Forwarded-For'
+        pour indiquer l'adresse IP de l'utilisateur à l'origine de la requête
+        proxifiée (à des fins de traçabilité/imputation).
+    @type headers: C{dict}
+    @return: Renvoie le résultat de la requête proxifiée, tel que retourné
+        par urllib2.
+    @rtype: C{file-like}
+    """
+
+    def _get_server(hostname):
+        """
+        Étant donné le nom d'un hôte du parc, cette méthode
+        renvoie le nom de domaine qualifié (FQDN) du serveur
+        de type C{server_type} responsable de cet hôte.
+
+        @param hostname: Nom d'hôte.
+        @type  hostname: C{unicode}
+
+        @return: FQDN du serveur du type demandé responsable de cet hôte.
+        @rtype: C{unicode}
+        """
+        return DBSession.query(
+                    VigiloServer.name
+                ).join(
+                    (Ventilation, Ventilation.idvigiloserver ==
+                        VigiloServer.idvigiloserver),
+                    (Host, Host.idhost == Ventilation.idhost),
+                    (Application, Application.idapp == Ventilation.idapp),
+                ).filter(Host.name == hostname
+                ).filter(Application.name == server_type
+                ).scalar()
+
+    # Éventuellement, l'utilisateur demande une page
+    # qui se rapporte à un service particulier.
+    service = data.get('service')
+
+    data = urllib.urlencode(data)
+
+    if headers is None:
+        headers = {}
+
+    user = get_current_user()
+    supitemgroups = user.supitemgroups(False)
+
+    # On vérifie qu'il existe effectivement un hôte portant ce nom
+    # et configuré pour être supervisé par Vigilo.
+    idhost = DBSession.query(
+                Host.idhost
+            ).filter(Host.name == host
+            ).scalar()
+    if idhost is None:
+        message = _('No such monitored host: %s') % host
+        LOGGER.warning(message)
+        raise HTTPNotFound(message)
+
+    # On regarde si l'utilisateur a accès à l'hôte demandé.
+    perm = DBSession.query(
+                SupItemGroup.idgroup
+            ).join(
+                (SUPITEM_GROUP_TABLE, SUPITEM_GROUP_TABLE.c.idgroup ==
+                    SupItemGroup.idgroup),
+            ).outerjoin(
+                (LowLevelService, LowLevelService.idservice ==
+                    SUPITEM_GROUP_TABLE.c.idsupitem),
+            ).filter(SUPITEM_GROUP_TABLE.c.idgroup.in_(supitemgroups))
+
+    # Si en plus on a demandé un service particulier,
+    # alors on vérifie les permissions de l'utilisateur
+    # sur ce service.
+    if service is not None:
+        perm = perm.filter(
+                    or_(
+                        and_(
+                            LowLevelService.idhost == idhost,
+                            LowLevelService.servicename == service,
+                        ),
+                        SUPITEM_GROUP_TABLE.c.idsupitem == idhost,
+                    ),
+                )
+    else:
+        perm = perm.filter(
+                    or_(
+                        LowLevelService.idhost == idhost,
+                        SUPITEM_GROUP_TABLE.c.idsupitem == idhost,
+                    ),
+                )
+
+    # On traite le cas où l'utilisateur n'a pas les droits requis.
+    if perm.scalar() is None:
+        message = None
+        if service is not None:
+            message = _('Access denied to host "%(host)s" and '
+                        'service "%(service)s"') % {
+                            'host': host,
+                            'service': service,
+                        }
+        else:
+            message = _('Access denied to host "%s"') % host
+        LOGGER.warning(message)
+        raise HTTPForbidden(message)
+
+    # On vérifie que l'hôte est effectivement pris en charge.
+    # ie: qu'un serveur du parc héberge l'application server_type
+    # responsable de cet hôte.
+    vigilo_server = _get_server(host)
+    if vigilo_server is None:
+        message = _('No %(server_type)s server configured to '
+                    'monitor "%(host)s"') % {
+                        'server_type': server_type,
+                        'host': host,
+                    }
+        LOGGER.warning(message)
+        raise HTTPNotFound(message)
+
+    # Récupére les informations sur l'emplacement de l'application
+    # distante. Par défaut, on suppose que la connexion se fait en
+    # texte clair (http) sur le port standard (80).
+    app_path = config['app_path.%s' % server_type]
+    app_scheme = config.get('app_scheme.%s' % server_type, 'http')
+    app_port = config.get('app_port.%s' % server_type, 80)
+    app_port = int(app_port)
+
+    full_url = [app_scheme,
+                "%s:%d" % (vigilo_server, app_port),
+                "/%s/%s" % (app_path, url),
+                data,
+                ""]
+    full_url = urlparse.urlunsplit(full_url)
+
+    req = urllib2.Request(full_url, headers=headers)
+    res = urllib2.urlopen(req)
+    return res
+
+
 # pylint: disable-msg=R0201,W0232
 # - R0201: méthodes pouvant être écrites comme fonctions (imposé par TG2)
 # - W0232: absence de __init__ dans la classe (imposé par TG2)
@@ -35,6 +188,7 @@ def make_proxy_controller(base_controller, server_type, mount_point):
     @type base_controller: C{BaseController}
     @param server_type: Type d'application à "proxifier", il peut
         s'agir de "nagios" ou "rrdgraph".
+    @type server_type: C{basestring}
     @param mount_point: URL à laquelle le contrôleur est montée
         (par rapport au RootController de l'application).
         Ce paramètre est utilisé pour réécrire correctement les
@@ -56,42 +210,20 @@ def make_proxy_controller(base_controller, server_type, mount_point):
     class ProxyController(base_controller):
         """
         Contrôleur agissant comme un proxy vers Nagios ou RRDgraph.
+        Ce contrôleur utilise la méthode get_through_proxy de ce module
+        pour obtenir les documents.
         """
 
         # L'accès à ce contrôleur nécessite d'être identifié.
         allow_only = not_anonymous(_("You need to be authenticated"))
 
-        def _get_server(self, hostname):
-            """
-            Étant donné le nom d'un hôte du parc, cette méthode
-            renvoie le nom de domaine qualifié (FQDN) du serveur
-            de type C{server_type} responsable de cet hôte.
-
-            @param hostname: Nom d'hôte.
-            @type  hostname: C{unicode}
-
-            @return: FQDN du serveur du type demandé responsable de cet hôte.
-            @rtype: C{unicode}
-            """
-            return DBSession.query(
-                        VigiloServer.name
-                    ).join(
-                        (Ventilation, Ventilation.idvigiloserver == VigiloServer.idvigiloserver)
-                    ).join(
-                        (Host, Host.idhost == Ventilation.idhost)
-                    ).join(
-                        (Application, Application.idapp == Ventilation.idapp)
-                    ).filter(Host.name == hostname
-                    ).filter(Application.name == server_type
-                    ).scalar()
-
         @expose(content_type=CUSTOM_CONTENT_TYPE)
-        def default(self, host, *args, **kwargs):
+        def default(self, *args, **kwargs):
             """
             Cette méthode capture toutes les requêtes HTTP transmises
             au contrôleur puis les redirige vers le serveur Nagios ou
             RRDgraph (selon le paramètre C{server_type} donné à la factory)
-            responsable de l'hôte L{host}.
+            responsable de l'hôte donné.
 
             Si ce contrôleur est monté dans "/nagios/", un appel à
             "http://localhost/nagios/example.com/cgi-bin/status.cgi?a=b"
@@ -99,100 +231,21 @@ def make_proxy_controller(base_controller, server_type, mount_point):
             "example.com".
             
             Les paramètres de la requête (query string) sont automatiquement
-            transmis dans la requête au serveur Nagios (en POST).
-            
-            @param host: Nom de l'hôte du parc sur lequel on souhaite
-                obtenir des informations.
-            @type host: C{unicode}
-            @param args: Chemins additionnels de la requête (par exemple,
-                ['cgi-bin', 'status.cgi'] avec la requête ci-dessus).
-            @type args: C{list}
+            transmis dans la requête au serveur distant (en POST).
+
+            @param args: Parties du chemin. Si le dernier élément du chemin
+                de la requête possèdait une extension, elle est supprimée
+                par TurboGears et n'apparaît pas ici. Le premier élément
+                du chemin DOIT être le nom d'hôte vers lequel on proxifie.
+            @type args: C{tuple}
             @param kwargs: Paramètres additionnels de la requête
-                (le contenu de la query string).
+                (ie: la query string, sous forme de dictionnaire).
+                Ils seront passés en POST dans la requête proxifiée.
             @type kwargs: C{dict}
             """
 
-            user = get_current_user()
-            supitemgroups = user.supitemgroups(False)
-
-            # On vérifie qu'il existe effectivement un hôte portant ce nom
-            # et configuré pour être supervisé par Vigilo.
-            idhost = DBSession.query(
-                        Host.idhost
-                    ).filter(Host.name == host
-                    ).scalar()
-            if idhost is None:
-                message = _('No such monitored host: %s') % host
-                LOGGER.warning(message)
-                raise HTTPNotFound(message)
-
-            # Éventuellement, l'utilisateur demande une page
-            # qui se rapporte à un service particulier.
-            service = kwargs.get('service')
-
-            # On regarde si l'utilisateur a accès à l'hôte demandé.
-            perm = DBSession.query(
-                        SupItemGroup.idgroup
-                    ).join(
-                        (SUPITEM_GROUP_TABLE, SUPITEM_GROUP_TABLE.c.idgroup ==
-                            SupItemGroup.idgroup),
-                    ).outerjoin(
-                        (LowLevelService, LowLevelService.idservice ==
-                            SUPITEM_GROUP_TABLE.c.idsupitem),
-                    ).filter(SUPITEM_GROUP_TABLE.c.idgroup.in_(supitemgroups))
-
-            # Si en plus on a demandé un service particulier,
-            # alors on vérifie les permissions de l'utilisateur
-            # sur ce service.
-            if service is not None:
-                perm = perm.filter(
-                            or_(
-                                and_(
-                                    LowLevelService.idhost == idhost,
-                                    LowLevelService.servicename == service,
-                                ),
-                                SUPITEM_GROUP_TABLE.c.idsupitem == idhost,
-                            ),
-                        )
-            else:
-                perm = perm.filter(
-                            or_(
-                                LowLevelService.idhost == idhost,
-                                SUPITEM_GROUP_TABLE.c.idsupitem == idhost,
-                            ),
-                        )
-
-            # On traite le cas où l'utilisateur n'a pas les droits requis.
-            if perm.scalar() is None:
-                message = None
-                if service is not None:
-                    message = _('Access denied to host "%(host)s" and '
-                                'service "%(service)s"') % {
-                                    'host': host,
-                                    'service': service,
-                                }
-                else:
-                    message = _('Access denied to host "%s"') % host
-                LOGGER.warning(message)
-                raise HTTPForbidden(message)
-
-            # On vérifie que l'hôte est effectivement pris en charge.
-            # ie: qu'un serveur du parc héberge l'application server_type
-            # responsable de cet hôte.
-            vigilo_server = self._get_server(host)
-            if vigilo_server is None:
-                message = _('No %(server_type)s server configured to '
-                            'monitor "%(host)s"') % {
-                                'server_type': server_type,
-                                'host': host,
-                            }
-                LOGGER.warning(message)
-                raise HTTPNotFound(message)
-
-            # Préparation des paramètres pour la requête finale.
-            kwargs['host'] = host
-            data = urllib.urlencode(kwargs)
-            args = list(args)
+            host = args[0]
+            args = list(args[1:])
 
             # TurboGears supprime l'extension de la requête
             # car il peut effectivement des traitements différents
@@ -202,20 +255,6 @@ def make_proxy_controller(base_controller, server_type, mount_point):
             if pylons.request.response_ext and args:
                 args[-1] = args[-1] + pylons.request.response_ext
 
-            # Récupére les informations sur l'emplacement de l'application
-            # distante. Par défaut, on suppose que la connexion se fait en
-            # texte clair (http) sur le port standard (80).
-            app_path = config['app_path.%s' % server_type]
-            app_scheme = config.get('app_scheme.%s' % server_type, 'http')
-            app_port = config.get('app_port.%s' % server_type, 80)
-            app_port = int(app_port)
-
-            full_url = [app_scheme,
-                        "%s:%d" % (vigilo_server, app_port),
-                        "/%s/%s" % (app_path, '/'.join(args)),
-                        data,
-                        ""]
-            full_url = urlparse.urlunsplit(full_url)
 
             # Facilite la traçabilité sur le serveur distant.
             headers = {
@@ -230,8 +269,8 @@ def make_proxy_controller(base_controller, server_type, mount_point):
             if pylons.request.accept:
                 headers['Accept'] = pylons.request.accept.header_value
 
-            req = urllib2.Request(full_url, headers=headers)
-            res = urllib2.urlopen(req)
+            url = '/'.join(args)
+            res = get_through_proxy(server_type, host, url, kwargs, headers)
 
             # On recopie les en-têtes de la réponse du serveur distant
             # dans notre propre réponse. Cette étape est particulièrement
@@ -246,7 +285,8 @@ def make_proxy_controller(base_controller, server_type, mount_point):
             if info['Content-Type'] == 'text/html':
                 orig_url = config['app_path.%s' % server_type]
                 # Le str() est obligatoire, sinon exception 
-                # "AttributeError: You cannot access Response.unicode_body unless charset is set"
+                # "AttributeError: You cannot access Response.unicode_body
+                #  unless charset is set"
                 dest_url = str('%s%s/' % (tg.url(mount_point), host))
                 doc = doc.replace(orig_url, dest_url)
             return doc
