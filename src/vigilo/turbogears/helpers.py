21e55c7e8cf2558059298b1ec334eaf92a2488d2
Aurelien BOMPARD <aurelien.bompard@c-s.fr>
2010-04-23 16:05:48 +0000

    Travail sur VigiMap et le widget client
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@2840 b22e2e97-25c9-44ff-b637-2e5ceca36478

--- src/vigilo/turbogears/helpers.py
+++ src/vigilo/turbogears/helpers.py
@@ -6,7 +6,10 @@ Bibliothèque de fonctions outils pour les applications utilisant TurboGears.
 from tg import request
 from vigilo.models.tables import User
 
-__all__ = ('get_current_user', )
+from vigilo.turbogears.units import convert_with_unit
+
+
+__all__ = ('get_current_user', 'get_readable_metro_value', )
 
 def get_current_user():
     """
@@ -32,3 +35,31 @@ def get_current_user():
         return None
     return User.by_user_name(userid)
 
+def get_readable_metro_value(host, ds):
+    """
+    Récupère et retourne une valeur "lisible" de métrologie, c'est à dire avec
+    trois chiffres significatifs et l'ordre de grandeur.
+
+    @param host: le nom d'hôte
+    @type  host: C{str}
+    @param ds: l'indicateur de métrologie
+    @type  ds: L{vigilo.models.tables.perfdatasource}
+    @return: un couple valeur entière, valeur en pourcentage 
+    @rtype:  C{tuple}
+    """
+    # doit être chargé après
+    from vigilo.turbogears.controllers.proxy import get_through_proxy
+
+    usage_url = "rrdgraph.py/getLastValue?host=%s&ds=%s" % (host, ds.name)
+    usage_req = get_through_proxy("rrdgraph", host, usage_url)
+    usage = usage_req.read()
+    try:
+        usage = float(usage)
+        percent = int(usage / float(ds.max) * 100)
+        usage = convert_with_unit(usage)
+    except (ValueError, TypeError):
+        usage = "?"
+        percent = "?"
+        logging.warning(_("Failed to get URL: %s") % url("/rrdgraph/%s/%s"
+                                  % (host, usage_url),qualified=True))
+    return (usage, percent)
