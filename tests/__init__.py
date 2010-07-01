bea3dc5abef17d74224291995434a3c321d5e28f
Francois POIROTTE <francois.poirotte@c-s.fr>
2010-07-01 07:41:40 +0000

    Correction de l'emplacement du fichier de config pour les tests.
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@3549 b22e2e97-25c9-44ff-b637-2e5ceca36478

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from vigilo.common.conf import settings
-settings.load_file('../settings_tests.ini')
+settings.load_file('settings_tests.ini')
 
 from vigilo.models.configure import configure_db
 configure_db(settings['database'], 'sqlalchemy_', None)
