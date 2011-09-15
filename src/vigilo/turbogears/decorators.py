e5e4752a53ef607fea4201f6e4d753798c10b2ba
Francois POIROTTE <francois.poirotte@c-s.fr>
2011-09-15 20:03:17 +0200

    Meilleure gestion des encodages dans VigiGraph
    - Utilisation de caractères accentués dans plusieurs tests pour mettre
      en évidence les éventuels problèmes liés à des conversions explicites
      ou implicites de/vers Unicode.
    - Correction des templates de VigiGraph pour gérer correctement les
      caractères accentués.
    - Ajout d'un pseudo-décorateur de pagination utilisant la même API que
      pour les autres (pseudo-)décorateurs de !TurboGears (nécessaire à
      cause d'un conflit entre le (vrai) décorateur "paginate" de
      !TurboGears et le pseudo-décorateur "validate".
      Voir ticket 2302 dans le trac de !TurboGears.
    - Utilisation de ce décorateur pour valider correctement les paramètres
      passés à la méthode searchHost du contrôleur rpc de VigiGraph.
    
    Change-Id: I209700c586943ba6b60a4f4f68940b33617c77ba
    Reviewed-on: https://vigilo-dev.si.c-s.fr/review/272
    Tested-by: Build system <qa@vigilo-dev.si.c-s.fr>
    Reviewed-by: Aurelien BOMPARD <aurelien.bompard@c-s.fr>

---
+++ src/vigilo/turbogears/decorators.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+
+from tg import tmpl_context
+from tg.decorators import Decoration
+from tg.configuration import Bunch
+from tg.util import partial
+from webhelpers.paginate import Page
+from pylons import request
+
+class paginate(object):
+    """Paginate a given collection.
+
+    This decorator is mainly exposing the functionality
+    of :func:`webhelpers.paginate`.
+
+    Repris de http://trac.turbogears.org/ticket/2302
+    """
+
+    def __init__(self, name, use_prefix=False,
+            items_per_page=10, max_items_per_page=0):
+        self.name = name
+        prefix = use_prefix and name + '_' or ''
+        self.page_param = prefix + 'page'
+        self.items_per_page_param = prefix + 'items_per_page'
+        self.items_per_page = items_per_page
+        self.max_items_per_page = max_items_per_page
+
+    def __call__(self, func):
+        decoration = Decoration.get_decoration(func)
+        decoration.register_hook('before_validate', self.before_validate)
+        decoration.register_hook('before_render', self.before_render)
+        return func
+
+    def before_validate(self, remainder, params):
+        page = params.pop(self.page_param, None)
+        if page:
+            try:
+                page = int(page)
+                if page < 1:
+                    raise ValueError
+            except ValueError:
+                page = 1
+        else:
+            page = 1
+        request.paginate_page = page or 1
+        items_per_page = params.pop(self.items_per_page_param, None)
+        if items_per_page:
+            try:
+                items_per_page = min(
+                    int(items_per_page), self.max_items_per_page)
+                if items_per_page < 1:
+                    raise ValueError
+            except ValueError:
+                items_per_page = self.items_per_page
+        else:
+            items_per_page = self.items_per_page
+        request.paginate_items_per_page = items_per_page
+        request.paginate_params = params.copy()
+        if items_per_page != self.items_per_page:
+            request.paginate_params[self.items_per_page_param] = items_per_page
+
+    def before_render(self, remainder, params, output):
+        if not isinstance(output, dict) or not self.name in output:
+            return
+        collection = output[self.name]
+        page = Page(collection, request.paginate_page,
+            request.paginate_items_per_page)
+        page.kwargs = request.paginate_params
+        if self.page_param != 'name':
+            page.pager = partial(page.pager, page_param=self.page_param)
+        if not getattr(tmpl_context, 'paginators', None):
+            tmpl_context.paginators = Bunch()
+        tmpl_context.paginators[self.name] = output[self.name] = page
