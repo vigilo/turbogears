bc6125b44d8d9fabfa1fd51025d6a270bae42d22
Aurelien BOMPARD <aurelien.bompard@c-s.fr>
2011-12-08 15:26:08 +0100

    Mise à jour des versions
    Utilisation de la commande :
    `vigilo-dev-update-versions -d . -s .../r2.17`
    
    BuildBot: no-build
    Change-Id: I4f494ffba573f613a3076472f7bc3014cdfede79

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ tests_require = []
 
 setup(
     name='vigilo-turbogears',
-    version='2.0.8',
+    version='2.0.9',
     author='Vigilo Team',
     author_email='contact@projet-vigilo.org',
     url='http://www.projet-vigilo.org/',
