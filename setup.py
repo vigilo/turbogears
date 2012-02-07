80f0465b805f7389c0252961975871f06631ef67
Aurelien BOMPARD <aurelien.bompard@c-s.fr>
2012-02-07 17:20:15 +0100

    Bump des versions (suite livraison un client 2.19)
    Change-Id: Ibc6459639546ce2a273f02c3c7a8b8a912122b80
    BuildBot: no-build

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ tests_require = []
 
 setup(
     name='vigilo-turbogears',
-    version='2.0.13',
+    version='2.0.14',
     author='Vigilo Team',
     author_email='contact@projet-vigilo.org',
     url='http://www.projet-vigilo.org/',
