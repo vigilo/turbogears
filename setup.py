aca7f7909bb550fa14e540306ba6ef98a2b56f8e
Thomas BURGUIERE <thomas.burguiere@c-s.fr>
2012-03-16 18:21:42 +0100

    Bump des versions (suite livraison r2)
    Change-Id: I29c54a96b320d325977984de998329678310a302
    BuildBot: no-build
    Reviewed-on: https://vigilo-dev.si.c-s.fr/review/593
    Reviewed-by: Francois POIROTTE <francois.poirotte@c-s.fr>
    Reviewed-by: Thomas BURGUIERE <thomas.burguiere@c-s.fr>
    Tested-by: Francois POIROTTE <francois.poirotte@c-s.fr>

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ tests_require = []
 
 setup(
     name='vigilo-turbogears',
-    version='2.1.0',
+    version='2.1.1',
     author='Vigilo Team',
     author_email='contact@projet-vigilo.org',
     url='http://www.projet-vigilo.org/',
