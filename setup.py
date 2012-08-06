62cdc93a38e921c0bbda1b2106d4577caa78943e
Francois POIROTTE <francois.poirotte@c-s.fr>
2012-08-06 09:15:22 +0200

    Bump des versions (suite à livraison r2.21).
    Change-Id: I523bc213f56ffdbb16ddac628a5260d6d07ec04e
    BuildBot: no-build
    Reviewed-on: https://vigilo-dev.si.c-s.fr/review/799
    Reviewed-by: Francois POIROTTE <francois.poirotte@c-s.fr>
    Tested-by: Francois POIROTTE <francois.poirotte@c-s.fr>

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ tests_require = []
 
 setup(
     name='vigilo-turbogears',
-    version='2.0.16',
+    version='2.0.17',
     author='Vigilo Team',
     author_email='contact@projet-vigilo.org',
     url='http://www.projet-vigilo.org/',
