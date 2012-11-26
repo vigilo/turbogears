caa08c47c5e237889c2c25cec08b51cdada88630
Francois POIROTTE <francois.poirotte@c-s.fr>
2012-11-26 14:26:26 +0100

    Monkey-patch init des contrôleurs.
    Patche à chaud la méthode `__init__` de la classe DecoratedController
    afin de ne pas générer d'erreurs liées aux passages d'arguments à la
    méthode `__init__` de la classe object sous Python 2.6 et plus.
    
    Change-Id: I951007cd32d9dc66d13f0bf7b464ae4b2f417f65
    Reviewed-on: https://vigilo-dev.si.c-s.fr/review/987
    Tested-by: Build system <qa@vigilo-dev.si.c-s.fr>
    Reviewed-by: Thomas BURGUIERE <thomas.burguiere@c-s.fr>

--- src/vigilo/turbogears/controllers/__init__.py
+++ src/vigilo/turbogears/controllers/__init__.py
@@ -4,8 +4,9 @@
 
 """Contrôleurs communs à plusieurs applications."""
 
-from tg import TGController, tmpl_context, request, i18n
+from tg import tmpl_context, request, i18n, controllers
 from tg.render import render
+from tg.decorators import allow_only
 from pylons.i18n import _, ungettext, N_
 from tw.api import WidgetBunch
 from pkg_resources import resource_filename
@@ -60,7 +61,24 @@ def set_temporary_lang(languages):
     pylons.c.l_ = themes_translator
 i18n.set_temporary_lang = set_temporary_lang
 
-class BaseController(TGController):
+def _controller_init(self, *args, **kwargs):
+    """
+    Surcharge la création d'un contrôleur pour éviter
+    une erreur liée au passage d'arguments à la méthode
+    __init__ de la classe object depuis Python 2.6.
+    """
+    if hasattr(self, 'allow_only') and self.allow_only is not None:
+        # Let's turn Controller.allow_only into something useful for
+        # the @allow_only decorator.
+        predicate = self.allow_only
+        self = allow_only(predicate)(self)
+    # DecoratedController est un descendant de la classe object.
+    # Depuis Python 2.6, une erreur est levée si on tente de passer
+    # des paramètres à la méthode __init__() de object.
+    super(controllers.DecoratedController, self).__init__()
+controllers.DecoratedController.__init__ = _controller_init
+
+class BaseController(controllers.TGController):
     """
     Base class for the controllers in the application.
 
@@ -76,4 +94,4 @@ class BaseController(TGController):
 
         request.identity = request.environ.get('repoze.who.identity')
         tmpl_context.identity = request.identity
-        return TGController.__call__(self, environ, start_response)
+        return controllers.TGController.__call__(self, environ, start_response)
