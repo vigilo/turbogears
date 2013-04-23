e7469e68b19212cbdc38235b43acda8e5cb65b6b
Thomas BURGUIERE <thomas.burguiere@c-s.fr>
2013-04-23 10:49:35 +0200

    Changement version 3.1
    Change-Id: Iea25fd5896b60d850d289ae4eb52a13f97a9adb1
    Reviewed-on: https://vigilo-dev.si.c-s.fr/review/1137
    Reviewed-by: Thomas BURGUIERE <thomas.burguiere@c-s.fr>
    Tested-by: Thomas BURGUIERE <thomas.burguiere@c-s.fr>

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ tests_require = []
 
 setup(
     name='vigilo-turbogears',
-    version='3.0.1',
+    version='3.1',
     author='Vigilo Team',
     author_email='contact@projet-vigilo.org',
     url='http://www.projet-vigilo.org/',
