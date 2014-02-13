bd48a206791472a727155842a003b8cd2c59350d
Thomas BURGUIERE <thomas.burguiere@c-s.fr>
2014-02-13 15:38:56 +0100

    Bump des versions (livraison r3.4)
    Change-Id: Ifa118ba2e6896f30e7080971f6d504b66e0084d6
    BuildBot: no-build
    Reviewed-on: https://vigilo-dev.si.c-s.fr/review/1370
    Reviewed-by: Thomas BURGUIERE <thomas.burguiere@c-s.fr>
    Tested-by: Thomas BURGUIERE <thomas.burguiere@c-s.fr>

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ tests_require = []
 
 setup(
     name='vigilo-turbogears',
-    version='3.3',
+    version='3.4',
     author='Vigilo Team',
     author_email='contact@projet-vigilo.org',
     url='http://www.projet-vigilo.org/',
