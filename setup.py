6cb7181592551580f3015b3173ae868aeef2ec86
Francois POIROTTE <francois.poirotte@c-s.fr>
2017-09-14 12:32:13 +0200

    Bump v4.1.0 (finale)
    Change-Id: I812e125099e3494342ffc3a1204ed35a898fd669
    BuildBot: no-build

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ tests_require = [
 
 setup(
     name='vigilo-turbogears',
-    version='4.1.0rc3',
+    version='4.1.0',
     author='Vigilo Team',
     author_email='contact.vigilo@c-s.fr',
     url='http://www.vigilo-nms.com/',
