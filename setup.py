2f579c24106995ad6c44261ec0474621a1dc32f7
Francois POIROTTE <francois.poirotte@c-s.fr>
2018-08-06 18:00:48 +0200

    Préparation Vigilo v5.0.0
    Change-Id: I5b54d21671dad42a51d3f56a71c9db787b21c23e

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ tests_require = [
 
 setup(
     name='vigilo-turbogears',
-    version='4.1.0',
+    version='5.0.0',
     author='Vigilo Team',
     author_email='contact.vigilo@c-s.fr',
     url='https://www.vigilo-nms.com/',
