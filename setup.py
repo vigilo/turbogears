8cdcd56a709b45c4b6aa6e73eb1ed8c48b88ed9f
Francois POIROTTE <francois.poirotte@c-s.fr>
2019-07-10 18:23:12 +0200

    Bump version v5.1.0alpha2
    Change-Id: I955c93e23322e0a782f0111cf59d6858a4ce0f75
    BuildBot: no-build

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ tests_require = [
 
 setup(
     name='vigilo-turbogears',
-    version='5.1.0a1',
+    version='5.1.0a2',
     author='Vigilo Team',
     author_email='contact.vigilo@c-s.fr',
     url='https://www.vigilo-nms.com/',
