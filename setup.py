97494845e5ee8726f6bf8a6ac2cd570d9d1eb890
Francois POIROTTE <francois.poirotte@c-s.fr>
2020-01-30 09:59:31 +0100

    Bump v5.0.1
    Change-Id: Ie8ab8cf4dc2228e6a4c2622f19e9d49cb01c1f1b

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ tests_require = [
 
 setup(
     name='vigilo-turbogears',
-    version='5.0.0',
+    version='5.0.1',
     author='Vigilo Team',
     author_email='contact.vigilo@c-s.fr',
     url='https://www.vigilo-nms.com/',
